fa2e5e9027494fcc0ac1e141c72e45c3a456bdcd
Corey Tarpley <developertarpley@gmail.com>
2024-09-19 18:09:28 -0700

    created greet-person func. in main.py along with tests

--- githubWorkflow/main.py
+++ githubWorkflow/main.py
@@ -2,5 +2,9 @@
 def hello_world():
     return "Hello, from Corey Tarpley!"
 
+def greet_person(name: str) -> str:
+    return "Hello, " + name + "!"
+
 if __name__ == "__main__":
     print(hello_world())
+    print(greet_person("Corey"))
